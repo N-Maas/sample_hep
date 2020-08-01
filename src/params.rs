1257f4eeffeb597241d43c6391b365e831197311
Nikolai Maas <nikolai.maas@student.kit.edu>
2020-08-01 22:37:08 +0200

    extended params

--- src/params.rs
+++ src/params.rs
@@ -1,6 +1,8 @@
 /// Tuning parameters
-// TODO: currently at most 32 possible for _K due to array limitations
-pub const _K: usize = 32;
+pub const _K: usize = 128;
 pub const _M: usize = 256;
+
+pub const _SCALING: usize = _K / 2;
+pub const _SAMPLING: usize = 8;
 
 const_assert!(_K.is_power_of_two());
