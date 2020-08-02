1c16cac29f85da03fc4ea97e3b01fc0a56a85a00
Nikolai Maas <nikolai.maas@student.kit.edu>
2020-08-02 22:03:54 +0200

    deletion heap overflow implementation

--- src/heap.rs
+++ src/heap.rs
@@ -1,4 +1,5 @@
 use crate::groups::*;
+use crate::params::*;
 use crate::primitives::*;
 use smallvec::SmallVec;
 use std::iter;
@@ -66,6 +67,7 @@ impl<T: Ord + Clone> Groups<T> {
         todo!();
     }
 
+    // TODO: structure checks
     fn insert_all(&mut self, iter: impl Iterator<Item = T>) {
         for el in iter {
             let idx = self.r_distr.distribute(&el);
@@ -92,7 +94,48 @@ impl<T: Ord + Clone> Groups<T> {
 
     // TODO: use quickselect or a sampled element instead of sorting?
     fn handle_deletion_heap_overflow(&mut self, remaining: T) {
-        todo!();
+        let max_seq_len = _M;
+
+        let mut elements: Vec<T> = self
+            .deletion_heap
+            .drain()
+            .chain(iter::once(remaining))
+            .collect();
+        elements.sort();
+        debug_assert!(elements.len() == _M + 1);
+
+        if self.group_list.is_empty() {
+            let step = _M as f64 / (_K + 1) as f64;
+            let first = step.round() as usize - 1;
+            let splitters: Vec<T> = (2..=_K)
+                .map(|i| elements[(i as f64 * step).round() as usize - 1].clone())
+                .collect();
+            let mut base_group = BaseGroup::new(max_seq_len, &splitters);
+            base_group.forced_insert_all(elements.drain((first + 1)..elements.len()));
+            self.group_list
+                .push(Box::new(BufferedGroup::from_base_group(base_group)));
+
+            let splitter = elements[first].clone();
+            self.r_distr.insert_splitter(splitter);
+        } else {
+            let mid = elements.len() / 2;
+            let seq = self.group_list.first_mut().unwrap().first_or_insert();
+            for el in elements.drain((mid + 1)..elements.len()) {
+                seq.push(el);
+            }
+            if seq.len() > max_seq_len {
+                self.handle_group_overflow(0, 0);
+            }
+
+            let splitter = elements[mid].clone();
+            debug_assert!(splitter <= *self.r_distr.splitter_at(0));
+            self.r_distr.replace_splitter(splitter, 0);
+        }
+
+        for el in elements.into_iter() {
+            // can not fail as the heap was emptied
+            self.deletion_heap.push(el).ok().unwrap();
+        }
     }
 
     fn handle_group_overflow(&mut self, group_idx: usize, seq_idx: usize) {
