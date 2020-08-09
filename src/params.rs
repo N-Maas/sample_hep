6497e0873fa7e6ca59187224777d9d16079f3bae
Nikolai Maas <nikolai.maas@student.kit.edu>
2020-08-09 19:44:03 +0200

    smaller params

--- src/params.rs
+++ src/params.rs
@@ -2,14 +2,14 @@
 #[cfg(test)]
 pub const _K: usize = 8;
 #[cfg(not(test))]
-pub const _K: usize = 128;
+pub const _K: usize = 4;
 #[cfg(test)]
 pub const _M: usize = 16;
 #[cfg(not(test))]
-pub const _M: usize = 256;
+pub const _M: usize = 8;
 
 pub const _SCALING: usize = _K / 2;
-pub const _SAMPLING: usize = 5;
+pub const _SAMPLING: usize = 3;
 
 const_assert!(_K.is_power_of_two());
 const_assert!(_SAMPLING % 2 == 1);
